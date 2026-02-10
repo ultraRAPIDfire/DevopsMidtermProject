0bb76d4d25416be1c3c2ee2ae6525a8881144528
JC Reyes <jcreyes078@gmail.com>
2026-02-10 16:13:33 +0800

    Final Deliverable: Fixed AttributeError and optimized CI environment

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,11 @@
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from scripts.processor import process_images
 
+
 def test_pipeline_execution():
     process_images()
-    assert len(os.listdir('outputs/')) > 0
+    assert os.path.exists("outputs")
+    assert len(os.listdir("outputs")) > 0
